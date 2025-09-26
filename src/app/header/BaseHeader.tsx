44d21bfc23fc2a97352d6a312698a3e8c806084d
Zhanna <kezhanna.03@mail.ru>
2025-09-27 01:31:10 +0500

    added header with bugs

---
+++ src/app/header/BaseHeader.tsx
@@ -0,0 +1,25 @@
+export default function BaseHeader() {
+    return (
+        <div className="max-w-[1164px] w-full mx-auto flex justify-between py-[8px] px-[0px] text-[14px]">
+            <div className="max-w-[140px] w-full  flex justify-between">
+        <div className="text-[#6f7a8d]">Kolesa.kz</div>
+        <div className="text-[#6f7a8d]">Krisha.kz</div>
+            </div>
+            <div className="max-w-[260px] w-full flex justify-between">
+                <div className="flex">
+                    <div className="text-[#1a7beb]">
+            Для бизнеса
+            </div>
+                    <img src="/" alt="" />
+       </div>
+        <div className="text-[#6f7a8d]">
+            Kolesa Гид
+        </div>
+                <div className="flex">
+                    <div className="text-[#6f7a8d]">RU</div>
+                    <img src="/" alt="" />
+            </div>
+            </div>
+            </div>
+    )
+}
---
+++ src/app/header/SecondHeader.tsx
@@ -0,0 +1,22 @@
+export default function SecondHeader() {
+    return (
+        <div className="max-w-[1164px] w-full mx-auto flex justify-between py-[12px]">
+            <div className="max-w-[950px] w-full flex justify-between text-[14px] ">
+             <img src="/" alt="" />
+                                <div className="flex bg-[#fff] rounded-[6px] py-[5px] px-[16px] border-1 border-[#1c18191a]">
+                    <img src="/" alt="" />
+                    <div className="text-[#1a7beb]">
+        Вход и регистрация     </div>
+                </div>
+            </div>
+            <div className="text-[14px] py-[5px] px-[16px]  bg-[#1a7beb] rounded-[6px]">
+                <div className="flex">
+                    <img src="/" alt="" />
+                    <div className="text-[#fff]">
+        
+        Подать объявление       </div>
+                </div>
+            </div>
+        </div>
+    )
+}
