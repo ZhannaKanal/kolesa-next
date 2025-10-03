74efdca6da3513212305a3b8bfe0092fd50440b1
Zhanna <kezhanna.03@mail.ru>
2025-10-03 23:54:39 +0500

    added addnew container's content

---
+++ src/app/main/AddNew.tsx
@@ -0,0 +1,15 @@
+export default function AddNew() {
+  return (
+    <section className="add-new max-w-[1164px] w-full mx-auto pb-[19.44px]">
+      <div className="add-new-link text-center">
+        <a className="text-[#2a81dd] text-[20px] underline" href="">
+          Подать объявление
+        </a>
+      </div>
+      <p className="add-new-info text-center text-[#666666] text-[11px] mt-[8.8128px]">
+        <span>Всего на сайте 1 088 138 объявлений,</span>
+        <span>из них 129 778 объявлений подано сегодня.</span>
+      </p>
+    </section>
+  );
+}
--- src/app/main/Main.tsx
+++ src/app/main/Main.tsx
@@ -1,7 +1,11 @@
 export default function Main() {
   return <div>
     <HotHeaderClearfix></HotHeaderClearfix>
+    <HotBodyHot></HotBodyHot>
+    <AddNew></AddNew>
   </div>;
 }
 
 import HotHeaderClearfix from "./HotHeaderClearfix";
+import HotBodyHot from "./HotbodyHot";
+import AddNew from "./AddNew";
