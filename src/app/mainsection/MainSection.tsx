a00c92180a262e1f925c761ca246bbe6399412a3
Zhanna <kezhanna.03@mail.ru>
2025-10-04 23:15:08 +0500

    set grid width for maincontent elements

--- src/app/mainsection/MainSection.tsx
+++ src/app/mainsection/MainSection.tsx
@@ -1,5 +1,7 @@
 export default function MainSection() {
-  return <div className="max-w-[1164px] w-full mx-auto bg-[red]">
-    new
-  </div>;
+  return (
+    <div className="max-w-[1164px] w-full mx-auto bg-[red] grid grid-cols-[2fr_1fr] gap-4">
+      
+    </div>
+  );
 }
