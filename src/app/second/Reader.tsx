a392d83ebf68b9b58f6ce2905aa2b4214973ca06
Zhanna <kezhanna.03@mail.ru>
2025-10-04 23:56:50 +0500

    added links/href of reader

--- src/app/second/Reader.tsx
+++ src/app/second/Reader.tsx
@@ -2,15 +2,23 @@ export default function Reader() {
   return (
     <div className="mt-[20px]">
       <h4 className="reader-heading mb-[20px] text-[20px] text-[#2A81dd] underline">
-        Новости
+        <a href="https://kolesa.kz/content/news/">Новости</a>
       </h4>
       <div className="record-items">
-        <div className="flex justify-start items-center">
-          <img className="mr-[16px] w-[69px] h-[48px]" src="https://kolesa-photos.kcdn.online/kolesa-read/670c8d99d7271ec4e49486e673319f9836093df9-230x160.jpg" alt="" />
+        {" "}
+        <a
+          className="flex justify-start items-center"
+          href="https://kolesa.kz/content/news/aktsiz-na-roskoshnye-avto-v-2026-godu-kak-rasschitat-i-platit/"
+        >
+          <img
+            className="mr-[16px] w-[69px] h-[48px]"
+            src="https://kolesa-photos.kcdn.online/kolesa-read/670c8d99d7271ec4e49486e673319f9836093df9-230x160.jpg"
+            alt=""
+          />
           <span className="text-[#2a81dd] underline max-w-[250px]">
             Акциз на роскошные авто в 2026 году. Как рассчитать и платить
           </span>
-        </div>
+        </a>
         <div className="mt-[12px]">
           {" "}
           Станут ли налоговики контролировать честность заполнения документов
@@ -18,13 +26,19 @@ export default function Reader() {
         </div>
       </div>
       <div className="mt-[12px] text-[#2A81dd] underline">
-        <a href="">Lada Vesta вылетела с дороги из-за блокировки руля</a>
+        <a href="https://kolesa.kz/content/news/lada-vesta-vyletela-s-dorogi-iz-za-blokirovki-rulya/">
+          Lada Vesta вылетела с дороги из-за блокировки руля
+        </a>
       </div>
       <div className="mt-[12px] text-[#2A81dd] underline">
-        <a href="">Новый Kia Telluride станет мощным гибридом</a>
+        <a href="https://kolesa.kz/content/news/novyj-kia-telluride-stanet-moshhnym-gibridom/">
+          Новый Kia Telluride станет мощным гибридом
+        </a>
       </div>
       <div className="mt-[12px] text-[#2A81dd] underline">
-        <a href="">Второй AUDI без четырёх колец заметили в Китае</a>
+        <a href="https://kolesa.kz/content/news/vtoroj-audi-bez-chetyryoh-kolets-zametili-v-kitae/">
+          Второй AUDI без четырёх колец заметили в Китае
+        </a>
       </div>
     </div>
   );
