370c0790e3984253c7ed9a218b28c771c5687a60
Zhanna <kezhanna.03@mail.ru>
2025-10-05 23:29:26 +0500

    added Partnerslinkblock

--- src/app/first/First.tsx
+++ src/app/first/First.tsx
@@ -1,5 +1,6 @@
 export default function First() {
-    return <div>
-        /
+    return <div className="px-[16px]">
+        <PartnersLink></PartnersLink>
     </div>
 }
+import PartnersLink from "./PartnersLink"
---
+++ src/app/first/PartnersLink.tsx
@@ -0,0 +1,9 @@
+export default function PartnersLink() {
+  return (
+    <section className="partners-links-block section-links-block mt-[24px] mb-[15px]">
+      <a className="text-[14px] text-[#2A81dd] underline font-bold" href="">
+        Автомобили в Узбекистане
+      </a>
+    </section>
+  );
+}
