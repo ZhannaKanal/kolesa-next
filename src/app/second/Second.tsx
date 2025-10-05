df76d36f6e84719e6ce36ebffb85f26538c84a2b
Zhanna <kezhanna.03@mail.ru>
2025-10-05 23:22:30 +0500

    added other section for second container (in future will be get data from api)

--- src/app/second/Second.tsx
+++ src/app/second/Second.tsx
@@ -1,6 +1,9 @@
 export default function Second() {
     return <div className="px-[16px] text-[13px] text-[#1c1819]">
         <Reader></Reader>
+        <Reader></Reader>
+        <Reader></Reader>
+        <Reader></Reader>
         
     </div>
 }
